3b841d41a723c32f273517c914d6d078284269bc
AravindAshok1962 <aravindashok1510@gmail.com>
2023-04-26 13:26:02 +0530

    counterapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter,setcounter]= useState(0);
+  function incrementcount(){
+    setcounter (counter+1);
+  }
+  function decrementcount(){
+    setcounter (counter-1);
+  }
+  function reset(){
+    setcounter (0)
+  }
   return (
-    <div className="App">
+    <div 
+    style={{
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      border:'2px solid gray',
+      margin:'10px',
+      padding:'150px',
+      backgroundColor:'orange'
+    }}
+    className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3 className='hi'>Its You.......?</h3>
+        <h1
+        className='text-center'
+        >{counter}</h1>
+        <button className='b1'
+        style={{
+         // backgroundColor:'green',
+         // color:'white',
+          padding:'10px',
+          border:'2px solid gray',
+          margin:'4px'
+        }}
+        onClick={incrementcount}>+</button>&nbsp;
+        <button className='b2'
+         style={{
+          //backgroundColor:' blue',
+          //color:'white',
+          padding:'10px',
+          border:'2px solid gray',
+          margin:'4px'
+        }}
+        onClick={decrementcount}>-</button>&nbsp;
+        <button className='b3'
+         style={{
+          //backgroundColor:'red',
+          //color:'white',
+          padding:'10px',
+          border:'2px solid gray',
+          margin:'4px'
+        }}
+        onClick={reset}>Reset</button>
       </header>
+      
     </div>
   );
 }
